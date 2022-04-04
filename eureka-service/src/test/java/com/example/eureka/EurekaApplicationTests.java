5267fd5b6aefd48fc255328d5e40891293c715da
yarisong <kevinjjun@gmail.com>
2022-04-04 09:06:46 +0900

    SCG-Load Balancer develop

--- eureka-service/src/test/java/com/example/eureka/EurekaApplicationTests.java
+++ eureka-service/src/test/java/com/example/eureka/EurekaApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.ecommerce;
+package com.example.eureka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EcommerceApplicationTests {
+class EurekaApplicationTests {
 
 	@Test
 	void contextLoads() {
