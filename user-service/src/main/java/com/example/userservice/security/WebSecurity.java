6308df87021fab8392170317dfe903fbeafe505a
yarisong <kevinjjun@gmail.com>
2022-04-07 08:40:52 +0900

    userService-jpa,security

---
+++ user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -0,0 +1,20 @@
+package com.example.userservice.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests().antMatchers(".users/**").permitAll();
+
+        //html frame 나뉘어진 것을 허용
+        http.headers().frameOptions().disable();
+        //  super.configure(http);
+    }
+}
