0dab71dbc6afa9f610a53c6cb5a145537f16cb17
yarisong <kevinjjun@gmail.com>
2022-04-18 08:52:32 +0900

    AuthenticationFilter dev

--- user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -5,16 +5,30 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.servlet.Filter;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests().antMatchers(".users/**").permitAll();
+//        http.authorizeRequests().antMatchers(".users/**").permitAll();
+
+        http.authorizeRequests().antMatchers("/**")
+                .hasIpAddress("10.50.2.175")
+                .and()
+                .addFilter(getAuthenticationFilter());
 
         //html frame 나뉘어진 것을 허용
         http.headers().frameOptions().disable();
         //  super.configure(http);
+    }
+
+    private AuthenticationFilter getAuthenticationFilter() throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        authenticationFilter.setAuthenticationManager(authenticationManager());
+
+        return authenticationFilter;
     }
 }
